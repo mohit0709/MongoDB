c848597aab44b48cae8fd03dd601a1e3bf2482ed
mohit0709 <mohitaglawe16@gmail.com>
2024-06-23 04:52:24 +0530

    index file updated

--- index.js
+++ index.js
@@ -19,11 +19,35 @@ app.get('/user', async (req,res) => {
 
 //posting data 
 
+//using a famous middleware
+app.use(express.json());
+
 app.post('/user', async (req,res) => {
+    console.log(req.body);
     const db = await connectToDatabase();
     const collection = db.collection('example2');
-    const result = collection.insertOne({name:"Arada Guller",age:20});
-    res.json(result);
+    const result = collection.insertOne(req.body);
+    res.json('Updated');
+})
+
+//putting data
+
+app.put('/user/:name', async (req,res) => {
+    console.log(req.params);
+    const db = await connectToDatabase();
+    const collection = db.collection('example1');
+    let result = await collection.updateOne({name:req.params.name},{$set:req.body});
+    res.send('Updated');   
+})
+
+//to delete via postman or dynamically
+app.delete('/user/:name', async (req,res) => {
+
+    const db = await connectToDatabase();
+    const collection = db.collection('example1');
+    const userName = req.params.name;
+    collection.deleteOne({name:userName});
+    res.send('Deleted');
 })
 
 app.listen(4000);
